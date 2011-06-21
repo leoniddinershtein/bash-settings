7f306c549ae559da8c369d5032b57a1ecfa47530
Leonid Dinershtein <leonid.dinershtein@flatsoft.com>
2011-06-21 14:36:26 +0400

    updated editor to vim && added --color

--- desktop/editors.sh
+++ desktop/editors.sh
@@ -1,10 +1,4 @@
-export ASYNC_EDITOR="mate" 
-export EDITOR="mate -w" 
-export CVSEDITOR="mate -w" 
-export SVN_EDITOR="mate -w"
-
-# common typos
-alias mt='mate'
-alias amte='mate'
-alias atme='mate'
-alias mate.='mate .'
+export ASYNC_EDITOR="vim"
+export EDITOR="vim"
+export CVSEDITOR="vim"
+export SVN_EDITOR="vim"
